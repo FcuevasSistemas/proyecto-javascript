dd86985e83e6c37d149c05b3403ba6a8d6bcecd6
Federico <fcuevas.sistemas@gmail.com>
2022-03-24 20:50:36 -0300

    librerias js

--- js/carrito.js
+++ js/carrito.js
@@ -195,25 +195,6 @@ class Carrito {
         }
     }
 
-    //Calcular montos
-    calcularTotal(){
-        let productosLS;
-        let total = 0, igv = 0, subtotal = 0;
-        productosLS = this.obtenerProductosLocalStorage();
-        for(let i = 0; i < productosLS.length; i++){
-            let element = Number(productosLS[i].precio * productosLS[i].cantidad);
-            total = total + element;
-            
-        }
-        
-        igv = parseFloat(total * 0.18).toFixed(2);
-        subtotal = parseFloat(total-igv).toFixed(2);
-
-        document.getElementById('subtotal').innerHTML = "S/. " + subtotal;
-        document.getElementById('igv').innerHTML = "S/. " + igv;
-        document.getElementById('total').value = "S/. " + total.toFixed(2);
-    }
-
     obtenerEvento(e) {
         e.preventDefault();
         let id, cantidad, producto, productosLS;
